e25438ba0a7897809a645ca8b87da7753089856e
EduardoGMora <lalogomo2000@outlook.com>
2025-09-28 12:45:42 -0600

    finishing game 2d demo

--- mob.gd
+++ mob.gd
@@ -1,16 +1,9 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var mob_type = Array($AnimatedSprite2D.sprite_frames)
-	$AnimatedSprite2D.animation = mob_type.pick_random()
+func _ready():
+	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
+	$AnimatedSprite2D.animation = mob_types.pick_random()
 	$AnimatedSprite2D.play()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+func _on_VisibilityNotifier2D_screen_exited():
 	queue_free()
--- playerxd.gd
+++ playerxd.gd
@@ -1,17 +1,16 @@
 extends Area2D
 
+signal hit
 
 @export var speed = 400
 var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	pass
+	hide()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var velocity = Vector2.ZERO
+func _process(delta):
+	var velocity = Vector2.ZERO # The player's movement vector.
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("move_left"):
@@ -20,20 +19,32 @@ func _process(delta: float) -> void:
 		velocity.y += 1
 	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
-		
-	velocity = velocity.normalized() * speed
-	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "walk"
-		$AnimatedSprite2D.flip_v = false
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-		$AnimatedSprite2D.play()
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = "up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
 		$AnimatedSprite2D.play()
 	else:
 		$AnimatedSprite2D.stop()
+
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-	
+
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$Trail.rotation = 0
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		rotation = PI if velocity.y > 0 else 0
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	set_process(true)
+
+func _on_body_entered(body):
+	hit.emit()
+	hide()
+	$CollisionShape2D.set_deferred(&"disabled", true)
